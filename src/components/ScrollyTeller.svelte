1d837f4eefe62808b1c038b4b4e74f4271f5c0cc
tylerkurpanek <82012116+tylerkurpanek@users.noreply.github.com>
2024-03-10 11:32:33 -0700

    test

--- src/components/ScrollyTeller.svelte
+++ src/components/ScrollyTeller.svelte
@@ -1,5 +1,7 @@
 <script>
+  import Scroller from "@sveltejs/svelte-scroller";
 
+  let count, index, offset, progress;
 </script>
 
 <style>
@@ -36,3 +38,33 @@
     margin: 0 0 2em 0;
   }
 </style>
+
+<Scroller
+  top={0.0}
+  bottom={1}
+  threshold={0.5}
+  bind:count
+  bind:index
+  bind:offset
+  bind:progress
+>
+  <div class="background" slot="background">
+
+    <div class="progress-bars">
+      <p>current section: <strong>{index + 1}/{count}</strong></p>
+      <progress value={count ? (index + 1) / count : 0} />
+
+      <p>offset in current section</p>
+      <progress value={offset || 0} />
+
+      <p>total progress</p>
+      <progress value={progress || 0} />
+    </div>
+  </div>
+
+  <div class="foreground" slot="foreground">
+    <section>This is the first section.</section>
+    <section>This is the second section.</section>
+    <section>This is the third section.</section>
+  </div>
+</Scroller>
